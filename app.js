5ace6af0f6829f2e5d572f2393859606bc79334c
Kevin Kou <kevinkou0930@gmail.com>
2025-08-04 23:22:49 +0800

    Fix port number variable

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(errorHandlerMiddleware)
 // app.patch('api/v1/tasks/:id', tasks)     - update task
 // app.delete('api/v1/tasks/:id', tasks)    - delete task
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 const start = async() => {
   try {
