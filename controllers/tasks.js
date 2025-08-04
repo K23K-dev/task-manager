409a25e2872bf04de95d514b258019a6a04b8b35
Kevin Kou <kevinkou0930@gmail.com>
2025-08-04 23:59:59 +0800

    Debugs error-handler

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -12,17 +12,16 @@ const createTask = asyncWrapper(async (req, res) => {
   res.status(201).json({task}) 
 })
 
-const getTask = asyncWrapper(async (req, res) => {
+const getTask = asyncWrapper(async (req, res, next) => {
   const {id:taskID} = req.params
   const task = await Task.findOne({_id:taskID})
   if (!task) {
     return next(createCustomError(`No task with id: ${taskID}`, 404))
   }
   res.status(200).json({task})
-  res.json({id: req.params.id})
 })
 
-const deleteTask = asyncWrapper(async (req, res) => {
+const deleteTask = asyncWrapper(async (req, res, next) => {
   const {id:taskID} = req.params
   const task = await Task.findOneAndDelete({_id:taskID})
   if (!task) {
@@ -31,13 +30,12 @@ const deleteTask = asyncWrapper(async (req, res) => {
   res.status(200).json({task})
 })
 
-const updateTask = asyncWrapper(async (req, res) => {
+const updateTask = asyncWrapper(async (req, res, next) => {
   const {id:taskID} = req.params
   const task = await Task.findOneAndUpdate({_id:taskID}, req.body, {
     new: true,
     runValidators: true,
   })
-  res.status(200).json({id: taskID, data: req.body})
   if (!task) {
     return next(createCustomError(`No task with id: ${taskID}`, 404))
   }
